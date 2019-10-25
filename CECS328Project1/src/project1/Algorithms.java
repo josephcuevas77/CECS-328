614c6d57bda9a11321a7d88c0d7569d154844d89
Joseph Cuevas <38505353+josephcuevas77@users.noreply.github.com>
2019-10-25 13:35:42 -0700

    Update Algorithms.java

--- CECS328Project1/src/project1/Algorithms.java
+++ CECS328Project1/src/project1/Algorithms.java
@@ -2,7 +2,7 @@ package project1;
 
 public class Algorithms {
 	
-	private int[] array;
+private int[] array;
 	
 	public Algorithms(int[] array)
 	{
@@ -24,10 +24,10 @@ public class Algorithms {
 		return this.array;
 	}
 	
-	public int Solution1() {
+	public static int Solution1(int[] a) {
 		int maxSum = 0;
 		int thisSum;
-		int n = array.length;
+		int n = a.length;
 		
 		// i = the starting index of sum
 		for(int i = 0; i < n; i++)
@@ -39,7 +39,7 @@ public class Algorithms {
 				
 				for(int k = i; k <= j; k++)
 				{
-					thisSum += array[k];
+					thisSum += 	a[k];
 					
 					if(thisSum > maxSum)
 					{
@@ -51,4 +51,77 @@ public class Algorithms {
 		
 		return maxSum;
 	}
+	
+	public int Solution2(int[] a) {
+		int maxSum = 0; 
+		int thisSum = 0;
+		int n = array.length;
+		//i: starting index of sum 
+		for(int i=0; i< n; i++) {     
+			thisSum= 0;     
+			//compute all sums that begin with index i     
+			for(int j=i; j< n; j++){
+				thisSum += a[j];
+			}
+			}
+		 
+        if(thisSum > maxSum)              
+        	maxSum = thisSum;  
+	return maxSum; 
+	}
+	
+	public int Solution3(int a[], int left, int right) {
+		//Base case 1     
+		if(right == left)         
+			return a[left]; 
+		 
+	    //Base case 2
+		if(right == left+1)
+			return max(a[left],a[right],a[left]+a[right]) ;
+	 
+	     int mid = (left+right)/2;      
+	     //Find the MSS that occurs in the left half of a      
+	     int mss_left = Solution3(a,left,mid); 
+	 
+	     //Find the MSS that occurs in the right half of a     
+	     int mss_right = Solution3(a,mid+1,right); 
+	 
+	     //Find the MSS that intersects both the left and right halves
+	     double mss_middle = mss_junior_middle(a,left,mid,right);       
+	     
+	     return max(mss_left,mss_right,mss_middle) 
+	}
+	
+	double mss_junior_middle(int[] a,int left,int mid,int right) {     
+		double max_left_sum = Double.NEGATIVE_INFINITY;     
+		int sum = 0;     int i; 
+		for(i=mid; i >= left; i--)     {
+			sum += a[i]; 
+			if(sum > max_left_sum)
+				max_left_sum = sum;     
+			} 
+		double max_right_sum = Double.NEGATIVE_INFINITY;     
+		sum = 0; 
+		for(i=mid+1; i <= right; i++){ 
+			sum += a[i]; 
+			if(sum > max_right_sum)
+				max_right_sum = sum;     
+			}      
+		return max_left_sum + max_right_sum;
+	}
+	
+	public int Solution4(int a[]) {
+		int maxSum = 0; 
+		int thisSum = 0; 
+		int n = a.length;
+		 
+		for(int i=0; i< n; i++) {
+			thisSum += a[i];
+			if(thisSum > maxSum)
+				maxSum = thisSum;
+			else if(thisSum < 0)
+				thisSum = 0; 
+		} return maxSum; 
+	}
+ 
 }
